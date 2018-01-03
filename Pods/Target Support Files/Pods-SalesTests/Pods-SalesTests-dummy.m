ba15e649e6f3d468868e59f971d06024c815a120
Philip Starner <phil@twigtale.com>
2018-01-02 17:08:19 -0800

    Initial commit with firebase configured.

---
+++ Pods/Target Support Files/Pods-SalesTests/Pods-SalesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SalesTests : NSObject
+@end
+@implementation PodsDummy_Pods_SalesTests
+@end
---
+++ Pods/Target Support Files/Pods-SalesUITests/Pods-SalesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SalesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SalesUITests
+@end
